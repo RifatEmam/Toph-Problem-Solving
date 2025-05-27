d6b8b54b2b1cb7f33561844448b6510696f46b81
M.D Rifat Emam <153221770+RifatEmam@users.noreply.github.com>
2025-05-28 00:57:06 +0600

    02

---
+++ Toph2.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+ long long A;
+ long long B;
+ long long sum=0;
+ if(-2000000000< A && B < 2000000000 ){
+ scanf("%lld %lldd",&A,&B);
+ sum=A+B;
+ printf("%lld\n",sum);
+
+ }
+
+return 0;
+}
+
