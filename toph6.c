46dbb8a7228caf7d73babbedfe4eb28b9addad9a
M.D Rifat Emam <153221770+RifatEmam@users.noreply.github.com>
2025-05-28 01:06:19 +0600

    06

---
+++ toph6.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main(){
+
+int A;
+ scanf("%d",&A);
+if(0<A && A<100){
+
+    for(int i=1;i<=A;i++){
+            if ((A%i)==0){
+                printf("%d\n",i);
+
+            }
+
+
+
+
+
+    }
+
+}
+
+
+
+
+
+return 0;
+}
